9fd58d80d65248745eaaaf837dccbe4909c7981d
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-21 22:00:02 +0530

    themes-dir

--- index.php
+++ index.php
@@ -51,6 +51,7 @@ define('FILE_CONFIG', 'config.php');
 define('FILE_USERS', 'users.php');
 define('FILE_UPLOADS', 'uploads.php');
 define('FILE_ISSUES', 'issues.php');
+define('DIR_THEMES', 'dirname(__FILE__).'/public/themes/');
 
 ### Standart settings
 define('DEFAULT_COLOR', '#333333');
