5c6ccde4be213c853b8d76797a887d2cfcb808f1
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-30 22:12:13 +0530

    updated hardcodes

--- languages/Trad_chineese.class.php
+++ languages/Trad_chineese.class.php
@@ -136,7 +136,7 @@ class Trad {
 
  const S_SEARCH = '#12，@用户，单字...';
  const S_COPYRIGHT = '由 %name% 提供支援';
- const S_UPGRADE = '从<a href="'.RELEASE.'">GitHub</a> 取得最新版本到<a href="https://bugtrackr.github.io /wiki/upgrade/">升级</a>。 ';
+ const S_UPGRADE = '从<a href="'.RELEASE.'">GitHub</a> 取得最新版本到<a href="'.UPGRADE.'">升级</a>。';
 
  const S_LAST_UPDATES = '最近更新...';
 
