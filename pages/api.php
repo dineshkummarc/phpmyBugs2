3ef3a5109d3a2b80b47982232c3512be09dae9fc
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-30 22:11:21 +0530

    updated hardcodes

--- pages/api.php
+++ pages/api.php
@@ -46,7 +46,7 @@ function endApi( $returnValues, $httpStatus = 200 ){
 // check if configuration file exists
 if(!file_exists(DIR_DATABASE."config_api.php")){
 	$returns['status'] = 0;
-	$returns['statusDetails'] = "Configuration file is missing. Check out the documentation https://bugtrackr.github.io/api/ and example configuration https://github.com/bugtrackr/Nireus/blob/master/sample_config/config_api.php for the API.";
+	$returns['statusDetails'] = "Configuration file is missing. Check out the documentation ".API." and example configuration ".URL."/blob/master/sample_config/config_api.php for the API.";
 	endApi( $returns, 500 );
 }
 
